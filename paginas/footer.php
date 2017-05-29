a8218b30856bbf5aae0dacf6764d58bf02fa8af8
MrDanielG <thedaniel115@gmail.com>
2017-05-28 23:59:43 -0500

    Add files via upload

---
+++ paginas/footer.php
@@ -0,0 +1,18 @@
+
+<footer class="footer">
+    <div class="container-fluid">
+        <nav class="pull-left">
+            <ul>
+                <li>
+                    <a href="#">
+                        Contacto
+                    </a>
+                </li>
+
+            </ul>
+        </nav>
+        <p class="copyright pull-right">
+            &copy; <script>document.write(new Date().getFullYear())</script> xdxdxdxd
+        </p>
+    </div>
+</footer>
