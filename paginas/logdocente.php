1eaaeb97965faa2b29b12dbffc0be7594f48e9ee
MrDanielG <thedaniel115@gmail.com>
2017-05-30 23:00:42 -0500

    LOGIN BUGS
    Tha madafakin b.ugs dude

--- paginas/logdocente.php
+++ paginas/logdocente.php
@@ -10,16 +10,17 @@ if(isset($_SESSION['user_docente'])){      /*Permite revisar si el usuario ya es
     <title>Login Docente</title>
 		<meta charset="utf-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<link rel="stylesheet" href="assets/css/styles_login.css" media="screen">
 		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 		<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
 		<meta name="viewport" content="width=device-width" />
 		<link type="text/css" rel="stylesheet" href="assets/css/materialize.min.css"  media="screen"/>
-		<link rel="stylesheet" href="assets/css/styles_login.css" media="screen">
+
 
   </head>
-  <body>
+  <body id="body_docente">
 
-		<div class="centrar" align="center">
+		<div class="" align="center">
 
 				<div align="center" id="card">
 
@@ -29,8 +30,6 @@ if(isset($_SESSION['user_docente'])){      /*Permite revisar si el usuario ya es
 												<span class="card-title">Inicio de Sesión</span>
 
 												<form action="Login/login.php" method="post">
-														<!--Envia por get los valores "usr" y "pass"-->
-														<!--NOTA: Si quieres cambiar el método tienes que borrarlo, guardar y volver a escribir el nuevo método-->
 
 														<div align="center">
 																<img src="assets/img/1.1.fw.png" class="logo" heigh="50%" width="50%">
