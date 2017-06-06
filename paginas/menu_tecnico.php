145003036989508d51bdd255cd92c73f646a7ec4
MrDanielG <thedaniel115@gmail.com>
2017-06-06 16:01:39 -0500

    Pagina de Técnico
    Falta crear usuario de técnico
    Nota pa Ramiro: Los menuselect() a donde redireccionan? Para crear una
    página del técnico para aprobar las prácticas

---
+++ paginas/menu_tecnico.php
@@ -0,0 +1,46 @@
+<div class="sidebar" id="menulat" data-color="" data-image="assets/img/sidebar-6.1.jpg">
+
+     <div class="sidebar-wrapper">
+           <div class="logo">
+               <a href="#" class="simple-text">
+                   AdmiLab
+               </a>
+           </div>
+
+           <ul class="nav">
+             <li>
+               <a href="#" onclick="menuselect(1);" class="waves-effect waves-light menubtn">
+                 <i class="pe-7s-display1"></i>
+                 <p>Panel</p>
+               </a>
+             </li>
+
+             <li>
+               <a href="#" onclick="menuselect(2);" class="waves-effect menubtn waves-light" style="display:none;">
+                 <i class="pe-7s-clock"></i>
+                 <p>Horario</p>
+               </a>
+             </li>
+
+             <li>
+               <a href="#" onclick="menuselect(3);" class="waves-effect menubtn waves-light">
+                 <i class="pe-7s-note2"></i>
+                 <p>Aprobar Prácticas</p>
+               </a>
+             </li>
+
+             <li>
+               <a href="#" onclick="menuselect(4);" class="waves-effect menubtn waves-light">
+                 <i class="pe-7s-user" ></i>
+                 <p>Usuario</p>
+               </a>
+             </li>
+             <li>
+               <a href="#" onclick="menuselect(6);" class="waves-effect menubtn waves-light">
+                 <i class="pe-7s-config"></i>
+                 <p>Configuración</p>
+               </a>
+             </li>
+           </ul>
+     </div>
+   </div>
